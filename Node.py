204fa7fd5b4c46a671267d1e43fbaf85d147b9c4
GenericNerdyUsername <12198834-genericnerdyusername@users.noreply.gitlab.com>
2022-08-15 15:37:05 +0100

    Fix flake8 errors
    (cherry picked from commit 2be62bdf9a97c9b6923f1b590d735e76e04586cd)

--- Node.py
+++ Node.py
@@ -5,7 +5,7 @@ class Node:
 
     def __init__(self, name: str = '', description: str = '', status: str = ''):
         self.name = name  # The project's name, shown in the render
-        self.description = description # A brief description of this project
+        self.description = description  # A brief description of this project
         self.status = status  # Current status of this project
         self.dependencies = set()  # Nodes which 'self' has as dependencies
         self.dependants = set()  # Nodes with 'self' as a dependency
