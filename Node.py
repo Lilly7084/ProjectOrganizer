8c58548dadecb2af2971cb3d3514158399c6fef5
GenericNerdyUsername <12198834-genericnerdyusername@users.noreply.gitlab.com>
2022-08-15 11:16:56 +0100

    remove redundant ` or ''`
    (cherry picked from commit 05ca2eb8bf4d8f1b769fca90acfc1e276a401d2e)

--- Node.py
+++ Node.py
@@ -4,9 +4,9 @@ from __future__ import annotations
 class Node:
 
     def __init__(self, name: str = '', description: str = '', status: str = ''):
-        self.name = name or ''  # The project's name, shown in the render
-        self.description = description or ''  # A brief description of this project
-        self.status = status or ''  # Current status of this project
+        self.name = name  # The project's name, shown in the render
+        self.description = description # A brief description of this project
+        self.status = status  # Current status of this project
         self.dependencies = set()  # Nodes which 'self' has as dependencies
         self.dependants = set()  # Nodes with 'self' as a dependency
         self.position_x = 0
